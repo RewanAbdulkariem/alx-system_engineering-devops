d60c598230c322931245a379219e779716e45d5f
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-04-25 03:51:02 +0200

    trial to fix test case 3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
-# Using Puppet, install flask from pip3.
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+#install a flask from pip3
+exec { 'flask':
+  command => '/usr/bin/apt-get -y install flask -v 2.1.0',
 }
